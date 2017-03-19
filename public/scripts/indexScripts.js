d639f8b6f17f1559cf088d74e6ae5ae8e738815e
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-03-19 01:54:46 -0700

    did main page need to work on drawings view and error view

---
+++ public/scripts/indexScripts.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+  //handle the scrolling
+  $(document).on("scroll", function(){
+      //position is the current position of the scroll
+    var position = Math.round($(window).scrollTop());
+
+      //var position = $(window).scrollTop();
+      if(position < $('#about').offset().top + $('#about').height()){
+        $("ul li").removeClass("active")
+        $("ul li:nth-child(1)").addClass("active");
+      }else if(position < $('#portfolio').offset().top + $('#portfolio').height()){
+        $("ul li").removeClass("active")
+        $("ul li:nth-child(2)").addClass("active");
+      }else if(position < $('#weather').offset().top + $('#weather').height()){
+        $("ul li").removeClass("active")
+        $("ul li:nth-child(3)").addClass("active");
+      }else if(position <$('#contact').offset().top + $('#contact').height()){
+        $("ul li").removeClass("active")
+        $("ul li:nth-child(4)").addClass("active");
+      }
+      });
+
+  //handle the clicking of the nav links and making the proper "button" as active
+  $( "li > a" ).click(function() {
+    $("li > a").parent().removeClass("active");
+    $( this ).parent().addClass( "active" );
+  });
+
+  //handle when user hovers over link images
+  $( $('a img')).hover( function() {
+      $( this )
+        .toggleClass( "imgactive animated jello" )
+    })
+
+    //weather info
+    $.get("//ipinfo.io", function(response) {
+    var url2 = url + response.city;
+    $.getJSON(url2,myCallback, 'jsonp');
+}, "jsonp");
+
+    $("#temp").click(function() {
+      if(isFahrenheit){
+        $("#temp").html(ctemp +"&ordm;C");
+        isFahrenheit = false;
+      }else{
+        $("#temp").html(ftemp +"&ordm;F");
+        isFahrenheit = true;
+      }
+
+});
+
+  });
+
+
+//functions for weather
+var isFahrenheit = true;
+var ftemp;
+var ctemp;
+var url="//api.apixu.com/v1/current.json?key=727bca30b78a41bb9da55855171101&q=";
+
+var myCallback = function(data) {
+//data from api
+  ftemp = data.current.temp_f;
+  ctemp = data.current.temp_c;
+  $("#location").text(data.location.name);
+
+  $("#temp").html(ftemp +"&ordm;F");
+  $("#description").text(data.current.condition.text);
+  $("#icon").attr("src",data.current.condition.icon);
+};
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var drawings = require('./routes/drawings');
+var app = express();
+var path = require('path');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/',express.static(__dirname + '/public'));
+app.use('/drawings', drawings);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+app.listen(process.env.PORT || 3000, process.env.IP,function () {
+  console.log('Process running');
+});
