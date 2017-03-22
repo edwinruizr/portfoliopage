f6828d6ae003e36147202a024782a6cf1ce230a6
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-03-21 19:32:50 -0700

    took out href to dropdown and added favicon

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var drawings = require('./routes/drawings');
 var motorcycle = require('./routes/motorcycle');
 var app = express();
 var path = require('path');
+var favicon = require('serve-favicon'); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use('/',express.static(__dirname + '/public'));
 app.use('/drawings', drawings);
@@ -24,7 +28,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  res.locals.status = err.status;
   // render the error page
   res.status(err.status || 500);
   res.render('error');
