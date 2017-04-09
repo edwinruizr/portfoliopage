194edfecd2e0e3d5b2c030b6dca6fcffda3f718c
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-04-09 13:32:40 -0700

    minify js with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,15 @@
 // grab our gulp packages
 var gulp  = require('gulp'),
     gutil = require('gulp-util');
+var uglify = require('gulp-uglify')
 
 // create a default task and just log a message
 gulp.task('default', function() {
   return gutil.log('Gulp is running!')
+});
+
+gulp.task('scripts',function(){
+  gulp.src('src/scripts/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('public/scripts'));
 });
