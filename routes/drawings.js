c85e2c2a87f77e8efbe6805f4c4f72fb2ad8180e
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-04-10 08:36:06 -0700

    changed titles to more descriptive titles

--- routes/drawings.js
+++ routes/drawings.js
@@ -5,6 +5,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req.docs);
+  res.set({
+  'Content-Type': 'text/plain',
+  'Cache-Control': 'max-age:86400000'
+});
   res.render('drawings', { title: 'Drawings', docs: req.docs });
 });
 
