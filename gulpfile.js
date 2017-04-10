0ab77088824ce76d553a70262e96055596e7cc39
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-04-09 17:24:30 -0700

    minified ejs

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 // grab our gulp packages
 var gulp  = require('gulp'),
     gutil = require('gulp-util');
-var uglify = require('gulp-uglify')
+var uglify = require('gulp-uglify');
+var minifyejs = require('gulp-minify-ejs');
 
 // create a default task and just log a message
 gulp.task('default', function() {
@@ -14,4 +15,10 @@ gulp.task('scripts',function(){
   gulp.src('src/scripts/*.js')
   .pipe(uglify())
   .pipe(gulp.dest('public/scripts'));
+});
+
+gulp.task('ejs', function(){
+gulp.src('src/views/*.ejs')
+  .pipe(minifyejs())
+  .pipe(gulp.dest('views'));
 });
