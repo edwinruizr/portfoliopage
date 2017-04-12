91f4f6d715f189574dd67419954065b30b049cd1
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-04-12 12:46:26 -0700

    set header for cache control on motorcycle route

--- routes/motorcycle.js
+++ routes/motorcycle.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.setHeader("Cache-Control","max-age=604800");
   res.render('motorcycle', { title: 'Motorcycle Mechanics' });
 });
 
